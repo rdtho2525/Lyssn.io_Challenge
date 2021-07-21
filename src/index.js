cbd0c1bc4137ff1e6f6b03c38fff5700e4c5199f
Reggie Thompson <reggie.thompson4@gmail.com>
2021-07-21 11:12:26 -0600

    Replace therapist with user(s)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const therapists = require('../src/data.json');
+const users = require('../src/data.json');
 
-const therapistList = document.getElementById('therapistList');
+const usersList = document.getElementById('usersList');
 
 const formatNames = (name) => {
   return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
-const displayTherapists = () => {
-  const allTherapists = therapists.data.map(therapist => {
-    const lastName = formatNames(therapist.lname);
-    const firstName = formatNames(therapist.fname);
-    return `<li class="therapist-name">${lastName}, ${firstName}</li>`
+const displayUsers = () => {
+  const allTherapists = users.data.map(user => {
+    const lastName = formatNames(user.lname);
+    const firstName = formatNames(user.fname);
+    return `<li class="user-name">${lastName}, ${firstName}</li>`
   });
 
-  therapistList.innerHTML = allTherapists.join('');
+  usersList.innerHTML = allTherapists.join('');
 }
 
 window.addEventListener('load', () => {
-  displayTherapists();
+  displayUsers();
 })
