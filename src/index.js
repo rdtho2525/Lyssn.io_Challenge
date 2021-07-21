33c5f49fa0be7a00903554b91481a755d4b5d3a9
Reggie Thompson <reggie.thompson4@gmail.com>
2021-07-21 09:50:13 -0600

    Display list items as children of the unordered list element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-
-
 const therapists = require('../src/data.json');
 
-const centerBox = document.getElementById('centerBox');
+const therapistList = document.getElementById('therapistList');
 
 const formatNames = (name) => {
   return name.charAt(0).toUpperCase() + name.slice(1);
@@ -12,10 +10,10 @@ const displayTherapists = () => {
   const allTherapists = therapists.data.map(therapist => {
     const lastName = formatNames(therapist.lname);
     const firstName = formatNames(therapist.fname);
-    return `<li>${lastName}, ${firstName}</li>`
+    return `<li class="therapist-name">${lastName}, ${firstName}</li>`
   });
 
-  centerBox.innerHTML = allTherapists.join('');
+  therapistList.innerHTML = allTherapists.join('');
 }
 
 window.addEventListener('load', () => {
