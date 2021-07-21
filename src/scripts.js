f7a7f656a444088f65cb141c636fd28bbebff184
Reggie Thompson <reggie.thompson4@gmail.com>
2021-07-21 09:37:48 -0600

    Add index.js file to webpack entry file

--- src/scripts.js
+++ src/scripts.js
@@ -6,5 +6,6 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './index.js'
 
 console.log('This is the JavaScript entry file - your code begins here.');
